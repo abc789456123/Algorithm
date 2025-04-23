6423a44f2826b31fe12ecc519aa313f7272adffc
abc789456123 <blow1218@tukorea.ac.kr>
2025-04-23 14:46:17 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,8 +1,24 @@
 #include <iostream>
 #include <string>
-#include <cmath>
 
 using namespace std;
+
+long long Mpow(int num, int exp) {
+	int MOD = 1234567891;
+	long long res = 1;
+	if (exp > 1) {
+		for (int i = 0; i < exp;i++) {
+			res = (res * num) % MOD;
+		}
+	}
+	else if (exp == 1) {
+		return 31;
+	}
+	else {
+		return 1;
+	}
+	return res;
+}
 
 int main()
 {
@@ -11,10 +27,13 @@ int main()
 
 	cin >> len >> str;
 
-	int res = 0;
+	int MOD = 1234567891;
+
+	long long res = 0;
 
 	for (int i = 0; i < len; i++) {
-		res += (str[i] - 'a' + 1) * (pow(31, i));
+		res += (str[i] - 'a' + 1) * (Mpow(31, i))%MOD;
+		res %= MOD;
 	}
 	cout << res;
 }
