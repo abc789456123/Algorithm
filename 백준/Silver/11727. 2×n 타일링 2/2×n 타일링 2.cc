e49520707adebbf800cbcd19351c54604a37ab47
abc789456123 <blow1218@tukorea.ac.kr>
2025-05-09 17:02:30 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    //입력 받기
+    int T;
+    cin >> T;
+
+    vector<int> v(T);
+    v[0] = 1;
+    v[1] = 3;
+
+    for (int i = 2;i < T;i++) {
+        v[i] = (v[i - 1] + 2 * v[i - 2])%10007;
+    }
+    cout << v[T - 1];
+    return 0;
+}
+
+/*
+1   1
+2   2
+3   3
+4   5
+5   
+
+*/
