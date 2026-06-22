c92b0faf076b4fd8b50a7224b941b8e95e45f87d
Park Gwi Hyeon <blow1218@tukorea.ac.kr>
2026-06-22 23:23:17 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.01 ms, Memory: 4.88 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int price, int money, int count) {
+    long long answer = -1;
+    long long sum = 0;
+    
+    for(int i = 1;i<=count;i++){
+        sum += i*price;
+    }
+    
+    if(sum>money){
+        return sum-money;
+    }
+    else{
+        return 0;
+    }
+}
